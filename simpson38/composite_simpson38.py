fe8aef4f3848d130db576534c295b66271bad750
darthleonard <leolinuxmx@gmail.com>
2020-12-08 23:47:11 -0600

    composite simpson 3 8

---
+++ simpson38/composite_simpson38.py
@@ -0,0 +1,17 @@
+def f(x):
+    return -(x * x) - 2 * x + 3
+
+def calculate(a, b, n):
+    h = (float(b - a) / n)
+    summatory = f(a) + f(b)
+    for i in range(1, n):
+        summatory += (2 if i % 3 == 0 else 3) * f(a + i * h)
+    return (float( 3 * h) / 8 ) * summatory
+
+lower_limit = -1
+upper_limit = 1
+interval = 100
+
+result = calculate(lower_limit, upper_limit, interval)
+
+print(round(result, 6))
